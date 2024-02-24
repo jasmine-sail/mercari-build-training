f372d1a9c42d8ff369fc8f0139e0a1c66bf984f4
rihoriho <a.z.ss.1301@gmail.com>
2024-02-24 17:58:46 +0900

    updateSTEP3

--- go/app/main.go
+++ go/app/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/labstack/echo/v4"
@@ -25,6 +30,7 @@ type Response struct {
 type Item struct {
 	Name     string `json:"name"`
 	Category string `json:"category"`
+	Image    string `json:"image"`
 }
 
 type ItemList struct {
@@ -44,10 +50,41 @@ func addItem(c echo.Context) error {
 	var item Item
 	item.Name = c.FormValue("name")
 	item.Category = c.FormValue("category")
-	c.Logger().Infof("Receive item: %s, %s", item.Name, item.Category)
+	imageFile, err := c.FormFile("image")
 
 	//4.jsonファイルの読み込み
 	file, err := os.OpenFile("items.json", os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
+	}
+	defer file.Close()
+
+	//画像ファイルの読み込み
+	src, err := imageFile.Open()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
+	}
+	defer src.Close()
+
+	//hash化
+	hash := sha256.New()
+	if _, err := io.Copy(hash, src); err != nil {
+		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
+	}
+	hashString := hex.EncodeToString(hash.Sum(nil))
+	imageFilename := hashString + ".jpg"
+
+	savePath := filepath.Join(ImgDir, imageFilename)
+	dst, err := os.Create(savePath)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
+	}
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, src); err != nil {
+		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
+	}
+	file, err = os.OpenFile("items.json", os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
 	}
@@ -58,6 +95,7 @@ func addItem(c echo.Context) error {
 	if err := decoder.Decode(&itemlist); err != nil {
 		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
 	}
+	item = Item{Name: item.Name, Category: item.Category, Image: imageFilename}
 
 	// 6. step5でdecodeしたitemをstep3のitemに追加する
 	itemlist.Items = append(itemlist.Items, item)
@@ -74,8 +112,8 @@ func addItem(c echo.Context) error {
 	if err := encoder.Encode(itemlist); err != nil {
 		return c.JSON(http.StatusInternalServerError, Response{Message: err.Error()})
 	}
-
-	message := fmt.Sprintf("item received: %s,%s", item.Name, item.Category)
+	c.Logger().Infof("Receive item: %s, %s,%s", item.Name, item.Category, item.Image)
+	message := fmt.Sprintf("item received: %s,%s,%s", item.Name, item.Category, item.Image)
 	res := Response{Message: message}
 
 	return c.JSON(http.StatusOK, res)
@@ -97,6 +135,7 @@ func getItem(c echo.Context) error {
 	return c.JSON(http.StatusOK, getitem)
 }
 
+// e.GET("/image/:imageFilename", getImg)
 func getImg(c echo.Context) error {
 	// Create image path
 	//画像ファイルが保存されているディレクトリのパスImgDirとクライアントから送信された画像ファイルの名前
@@ -116,6 +155,25 @@ func getImg(c echo.Context) error {
 	//c.File()関数は、指定されたファイルパスに対応するファイルを
 	//クライアントに送信するためのレスポンスを作成
 	return c.File(imgPath)
+}
+
+// e.GET("/image/:id",getID )
+func getId(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	file, err := os.Open("items.json")
+	if err != nil {
+		c.Logger().Infof("Error message: %s", err)
+	}
+	defer file.Close()
+
+	iditem := ItemList{}
+
+	if err := json.NewDecoder(file).Decode(&iditem); err != nil {
+		c.Logger().Infof("Error message: %s", err)
+	}
+	defer file.Close()
+	return c.JSON(http.StatusOK, iditem.Items[id-1])
+
 }
 
 func main() {
@@ -140,6 +198,7 @@ func main() {
 	e.POST("/items", addItem)
 	e.GET("/items", getItem)
 	e.GET("/image/:imageFilename", getImg)
+	e.GET("/image/:id", getId)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":9000"))
